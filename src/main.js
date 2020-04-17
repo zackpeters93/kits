843c7cae577b73af86e41571936d5ca2b4c847cf
DevMac <zp@DevMac.local>
2020-04-17 11:23:04 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -2,10 +2,30 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+// import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { firestorePlugin } from "vuefire";
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
+Vue.use(firestorePlugin);
+
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  data: {
+    // use an array for collection
+    todos: [],
+    // and null for documents
+    currentTodo: null
+  },
+  firestore: {
+    // todos: db.collection("todos"),
+    // currentTodo: db.collection("todos").doc("1")
+  }
 }).$mount("#app");
